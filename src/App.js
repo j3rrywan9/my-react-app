e590d913269379e8a0978d49f8703cce9436bcd8
Jerry Wang <j3rry.wan9@gmail.com>
2020-11-08 21:20:27 -0800

    add Greeting component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Greeting from './Greeting';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Greeting />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
