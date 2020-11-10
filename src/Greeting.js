e5101c5dd30aa7b3dd730d403be1218eb786fc66
Jerry Wang <j3rry.wan9@gmail.com>
2020-11-09 17:17:20 -0800

    demonstrate React Fragments short syntax

--- src/Greeting.js
+++ src/Greeting.js
@@ -3,7 +3,19 @@ import React from 'react';
 function Greeting() {
     const isMorning = (new Date()).getHours < 12;
 
-    return isMorning ? <h3>Good Morning!</h3> : <h3>Good Evening!</h3>;
+    return isMorning
+      ? (
+        <>
+            <h3>Good Morning!</h3>)
+            <p>You have 5 new messages</p>
+        </>
+      )
+      : (
+        <>
+            <h3>Good Evening!</h3>
+            <p>You have 5 new messages</p>
+        </>
+      );
 }
 
 export default Greeting;
