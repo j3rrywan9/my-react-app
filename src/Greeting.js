3d8db80703a0b1d6043e1430d970d5727482e5d9
Jerry Wang <j3rry.wan9@gmail.com>
2020-11-11 00:34:29 -0800

    use destructuring assignment

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export const Greeting = (props) => {
+export const Greeting = ({ name, numberOfMessages }) => {
   const isMorning = (new Date()).getHours() < 12;
-  const greetingHeader = isMorning ? <h3>Good Morning {props.name}!</h3> : <h3>Good Evening {props.name}!</h3>;
+  const greetingHeader = isMorning ? <h3>Good Morning {name}!</h3> : <h3>Good Evening {name}!</h3>;
 
   return (
     <>
       {greetingHeader}
-      <p>You have {props.numberOfMessages} new messages</p>
+      <p>You have {numberOfMessages} new messages</p>
     </>
   );
 }
