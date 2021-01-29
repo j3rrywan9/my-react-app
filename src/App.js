c4e66e7ac5beac81d8d62566dd2e620b2709f28f
Jerry Wang <j3rry.wan9@gmail.com>
2021-01-28 22:31:08 -0800

    create a functional component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Greeting } from './Greeting';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Person />
     </div>
   );
 }
